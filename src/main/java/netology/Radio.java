12190982e7205882280588fbd4e1771ff0c8278a
evgenijerm <erm.evgeniy18@gmail.com>
2020-10-09 12:33:56 +0300

    lombok

--- src/main/java/netology/Radio.java
+++ src/main/java/netology/Radio.java
@@ -1,80 +1,19 @@
 package netology;
 
-public class Radio {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    private int currentStation;
-    final int minStationNumber = 0;
-    final int maxStationNumber = 10;
-    final int minSound = 0;
-    final int maxSound = 100;
-    private int currentSound;
 
-    // конструкторы
-    public Radio (){
-        this.currentStation = 10;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public class Radio {
+        private int currentStation;
+        final int minStationNumber = 0;
+        final int maxStationNumber = 10;
+        final int minSound = 0;
+        final int maxSound = 100;
+        private int currentSound;
     }
-
-    public Radio (int currentStation){
-        this.currentStation =currentStation;
-    }
-
-
-    public int getStationNumber() {
-        return currentStation;
-    }
-
-    public int getCurrentSound() {
-        return currentSound;
-    }
-
-    //увеличиваем громкость
-    public void increaseSound() {
-        this.currentSound++;
-        if (currentSound >= maxSound) {
-            currentSound = maxSound;
-        }
-    }
-
-    //уменьшаем громкость
-    public void decreaseSound() {
-        this.currentSound--;
-        if (currentSound <= minSound) {
-            currentSound = minSound;
-        }
-    }
-
-    //переключаем радиостанцию на следующую.
-    public void nextStation() {
-        this.currentStation++;
-        if (currentStation > maxStationNumber) {
-            currentStation = minStationNumber;
-        }
-    }
-
-    //переключаем радиостанцию на предыдущую
-    public void prevStation() {
-        this.currentStation--;
-        if (currentStation < minStationNumber) {
-            currentStation = maxStationNumber;
-
-        }
-    }
-
-    // выставляем станцию с цифрового пульта
-    public void setStation(int stationNumber) {
-        if (stationNumber > maxStationNumber || stationNumber < minStationNumber) {
-            System.out.println("Такой станции не существует");
-        } else {
-            currentStation = stationNumber;
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
