9357739079a24695fb8066e6dadebcbda4627a1f
evgenijerm <erm.evgeniy18@gmail.com>
2020-10-19 18:24:48 +0300

    тесты ломбок

---
+++ src/test/java/netology/RadioTest.java
@@ -0,0 +1,45 @@
+package netology;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+class RadioTest {
+
+    @org.junit.jupiter.api.Test
+    void getCurrentStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMinStationNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMaxStationNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMinSound() {
+    }
+
+    @Test
+    void getMaxSound() {
+    }
+
+    @Test
+    void getCurrentSound() {
+    }
+
+    @Test
+    void setCurrentStation() {
+    }
+
+    @Test
+    void setCurrentSound() {
+    }
+
+}
